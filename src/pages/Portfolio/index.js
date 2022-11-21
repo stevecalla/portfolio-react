95974525e1816df9cfda60b2497f3dc480b6abf4
Steve Calla <callasteven@gmail.com>
2022-11-20 19:49:59 -0700

    Update class name

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -14,37 +14,37 @@ const Portfolio = () => {
             </div>
             <section id="card-wrapper">
             <div>
-                <img src={imageProject3} class="project-pic" />
+                <img src={imageProject3} className="project-pic" />
                 <h5 className="project-header">Project 1</h5>
                 <a className="links" target="blank">Github link</a>
                 <a className="links" target="blank">Deployed page</a>
             </div>
             <div>
-                <img src={imageHealthTracker} class="project-pic" />
+                <img src={imageHealthTracker} className="project-pic" />
                 <h5 className="project-header">Health Tracker</h5>
                 <a href="https://github.com/heintze11/health-tracker" target="blank" className="links">Github link</a>
                 <a href="https://evening-reef-97935.herokuapp.com/" target="blank" className="links">Deployed link</a>
             </div>
             <div>
-                <img src={imageAirQuality} class="project-pic" />
+                <img src={imageAirQuality} className="project-pic" />
                 <h5 className="project-header">Air Quality Comparison</h5>
                 <a href="https://github.com/heintze11/health-tracker" target="blank" className="links">Github link</a>
                 <a href="https://evening-reef-97935.herokuapp.com/" target="blank" className="links">Deployed link</a>
             </div>
             <div>
-                <img src={codingQuiz} class="project-pic2" />
+                <img src={codingQuiz} className="project-pic2" />
                 <h5 className="project-header">Coding Quiz</h5>
                 <a href="https://github.com/thaling11/javascript-coding-quiz" target="blank" className="links">Github link</a>
                 <a href="https://thaling11.github.io/javascript-coding-quiz/#" target="blank" className="links">Deployed link</a>
             </div>
             <div>
-                <img src={techBlog} class="project-pic2" />
+                <img src={techBlog} className="project-pic2" />
                 <h5 className="project-header">Tech Blog</h5>
                 <a href="https://github.com/thaling11/MVC-tech-blog" target="blank" className="links">Github link</a>
                 <a href="https://mvc-tech-blog-th.herokuapp.com/" target="blank" className="links">Deployed link</a>
             </div> 
             <div>
-                <img src={noteTaker} class="project-pic2" />
+                <img src={noteTaker} className="project-pic2" />
                 <h5 className="project-header">Note Taker</h5>
                 <a href="https://github.com/thaling11/note-taker" target="blank" className="links">Github link</a>
                 <a href="https://note-taker-th.herokuapp.com/" target="blank" className="links">Deployed link</a>
